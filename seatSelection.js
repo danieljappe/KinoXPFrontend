3117c1d0ef843799e9edef2d9b6335679d074443
Nademtis <Mathias.99.madsen@gmail.com>
2023-10-04 11:47:50 +0200

    seat reservation in progress

---
+++ seatSelection.js
@@ -0,0 +1,39 @@
+function createSeatSelector(rows, seatsPerRow) {
+    const container = document.getElementById("seatDiv");
+    // const img = document.createElement("img")
+    // img.src = "seat.png"
+
+    const table = document.createElement('table');
+    table.style.width = '100%';
+    table.style.height = '100%';
+    table.border = '1';
+
+    for (let i = 1; i <= rows; i++) {
+        const row = document.createElement('tr');
+
+        for (let j = 1; j <= seatsPerRow; j++) {
+            const seat = document.createElement('td');
+            seat.style.backgroundColor = 'white';
+            const seatButton = document.createElement('button');
+            seatButton.style.backgroundColor = 'transparent';
+            seatButton.style.borderStyle = 'hidden';
+            // seatButton.innerText = `R${i}, S${j}`;
+            seatButton.innerHTML = "<img style='width: 50px;height: 50px' src='icons/seat.png'/>";
+            seatButton.addEventListener('click', () => {
+                alert(`You selected Seat ${j} in Row ${i}.`);
+            });
+            seat.appendChild(seatButton);
+            row.appendChild(seat);
+        }
+        table.appendChild(row);
+    }
+    container.appendChild(table);
+}
+
+// Call the function to create the seat selector table
+const smallTheatreRows = 20;
+const smallTheatreSeatsPerRow = 12;
+const bigTheatreRows = 25;
+const bigTheatreSeatsPerRow = 16;
+// createSeatSelector(smallTheatreRows, smallTheatreSeatsPerRow);
+createSeatSelector(bigTheatreRows, bigTheatreSeatsPerRow);
