3f7d3e6468befdd64e1cb667acde2eb764f2b592
fred077a <fred18q3@stud.kea.dk>
2023-10-11 15:27:28 +0200

    current movies uses movies from db

--- public/src/main.js
+++ public/src/main.js
@@ -1,17 +1,17 @@
 import Showing from "./models/showing.js";
 import { ShowingRepository } from "./repository/showing_repository.js";
 import { MovieRepository } from "./repository/movie_repository.js";
-import DateParser from "./utilities/date_parser.js";
+import { PopularMovies as CurrentMovies } from "./popular-movies/popular-movies.js";
 
-console.log("before");
+//repositories
 const showingRepository = new ShowingRepository();
 const movieRepository = new MovieRepository();
-/*const movs = repository.getAllMovies();
-for (let i = 0; i < movs.length; i++) console.log(movs[i]);*/
 
-const showings = await showingRepository.getAllShowings();
-const dateParser = new DateParser();
+//data
+const showingsNext3Months = await showingRepository.getAllShowings();
+const allMovies = await movieRepository.getAllMovies();
 
+/* //FOR TESTING
 const showingsToCreate = [
   new Showing(0, 1, 2, "2023-10-16T15:00:00", dateParser),
   new Showing(1, 2, 1, "2023-10-16T16:00:00", dateParser),
@@ -21,135 +21,25 @@ const showingsToCreate = [
 for (let i = 0; i < showingsToCreate.length; i++) {
   //showingRepository.createShowing(showingsToCreate[i]);
 }
+*/
 
-const allMovies = await movieRepository.getAllMovies();
-
-for (let i = 0; i < showings.length; i++) {
-  const movie = await movieRepository.getMovie(showings[i].movieId);
-  showings[i].movie = movie;
-  console.log(showings[i]);
-}
+//insert movies to 'current movies'-section
+const currentMovies = new CurrentMovies(allMovies);
+await currentMovies.show();
 
 
-const movies = [
-  {
-      id: 1,
-      title: "Inception",
-      description: "Dom Cobb, a skilled thief, and corporate espionage specialist is tasked with a unique job - to implant an idea into a person's mind through their dreams. As he assembles a team, they delve into layers of dreams within dreams, facing unexpected challenges and questioning the reality around them.",
-      genre: "Science Fiction/Thriller",
-      ageRestriction: 13,
-      runTime: 148,
-      trailerLink: "https://www.youtube.com/embed/8hP9D6kZseM",
-      posterLink: "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_.jpg"
-  },
-  {
-      id: 2,
-      title: "The Shawshank Redemption",
-      description: "Andy Dufresne, a banker wrongly accused of murder, is sentenced to life in Shawshank State Penitentiary. Within the harsh prison walls, he forms an unlikely friendship with fellow inmate Red. Together, they find hope and redemption in the most unlikely places.",
-      genre: "Drama/Crime",
-      ageRestriction: 17,
-      runTime: 142,
-      trailerLink: "https://www.youtube.com/embed/6hB3S9bIaco",
-      posterLink: "https://m.media-amazon.com/images/M/MV5BMTc3NjM4MTY3MV5BMl5BanBnXkFtZTcwODk4Mzg3OA@@._V1_.jpg"
-  },
-  {
-      id: 3,
-      title: "The Avengers",
-      description: "Earth's mightiest heroes, including Iron Man, Captain America, Thor, Hulk, and others, are assembled by S.H.I.E.L.D. to save the world from Loki and his alien army. As they join forces, they learn to work as a team, combining their unique abilities to battle a global threat.",
-      genre: "Action/Adventure",
-      ageRestriction: 13,
-      runTime: 143,
-      trailerLink: "https://www.youtube.com/embed/eOrNdBpGMv8",
-      posterLink: "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg"
-  },
-  {
-      id: 4,
-      title: "La La Land",
-      description: "Mia, an aspiring actress, and Sebastian, a jazz musician, pursue their dreams in the vibrant city of Los Angeles. As they face the challenges of a competitive industry, they discover the sacrifices and heartaches that come with following their passions.",
-      genre: "Musical/Romance",
-      ageRestriction: 13,
-      runTime: 128,
-      trailerLink: "https://www.youtube.com/embed/0pdqf4P9MB8",
-      posterLink: "https://m.media-amazon.com/images/M/MV5BZjYzNTMzZTAtNzMzOS00ZWRhLTg3MWMtM2NjYmJmZjU4NmFhL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyMzI3NjY2ODc@._V1_.jpg"
-  },
-  {
-      id: 5,
-      title: "The Grand Budapest Hotel",
-      description: "In the fictional Republic of Zubrowka, a quirky hotel concierge and his apprentice become embroiled in a series of bizarre adventures involving theft, murder, and a valuable Renaissance painting. Their escapades lead them on a madcap journey through a visually stunning world. In the fictional Republic of Zubrowka, a quirky hotel concierge and his apprentice become embroiled in a series of bizarre adventures involving theft, murder, and a valuable Renaissance painting. Their escapades lead them on a madcap journey through a visually stunning world. In the fictional Republic of Zubrowka, a quirky hotel concierge and his apprentice become embroiled in a series of bizarre adventures involving theft, murder, and a valuable Renaissance painting. Their escapades lead them on a madcap journey through a visually stunning world. In the fictional Republic of Zubrowka, a quirky hotel concierge and his apprentice become embroiled in a series of bizarre adventures involving theft, murder, and a valuable Renaissance painting. Their escapades lead them on a madcap journey through a visually stunning world.",
-      genre: "Comedy/Mystery",
-      ageRestriction: 17,
-      runTime: 100,
-      trailerLink: "https://www.youtube.com/embed/1Fg5iWmQjwk",
-      posterLink: "https://m.media-amazon.com/images/M/MV5BMzM5NjUxOTEyMl5BMl5BanBnXkFtZTgwNjEyMDM0MDE@._V1_FMjpg_UX1000_.jpg"
-  },
-  {
-      id: 6,
-      title: "Finding Nemo",
-      description: "Marlin, a clownfish, embarks on a journey across the ocean to find his son, Nemo, who has been captured by a diver and placed in a fish tank. Along the way, Marlin is joined by a forgetful fish named Dory, and together, they encounter various sea creatures and dangers.",
-      genre: "Animation/Family",
-      ageRestriction: 0,
-      runTime: 100,
-      trailerLink: "https://www.youtube.com/embed/wZdpNglLbt8",
-      posterLink: "https://m.media-amazon.com/images/M/MV5BYjZlNzkzNzctZTY0NS00YmZhLThjNWMtMTViYWNlM2IyMzZiXkEyXkFqcGdeQXVyODk1MjAxNzQ@._V1_.jpg"
-  },
-  {
-      id: 7,
-      title: "The Social Network",
-      description: "The film chronicles the founding of Facebook and the legal battles that ensued between its creators. As the social media platform gains immense popularity, friendships are tested, and personal ethics are questioned, leading to a dramatic legal showdown.",
-      genre: "Drama/Biography",
-      ageRestriction: 13,
-      runTime: 120,
-      trailerLink: "https://www.youtube.com/embed/lB95KLmpLR4",
-      posterLink: "https://m.media-amazon.com/images/M/MV5BOGUyZDUxZjEtMmIzMC00MzlmLTg4MGItZWJmMzBhZjE0Mjc1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg"
-  },
-  {
-      id: 8,
-      title: "Spirited Away",
-      description: "Chihiro, a young girl, becomes trapped in a mysterious and magical world. To rescue her parents and escape, she must work in a bathhouse run by supernatural beings, encountering strange creatures and undergoing personal growth along the way.",
-      genre: "Animation/Fantasy",
-      ageRestriction: 0,
-      runTime: 125,
-      trailerLink: "https://www.youtube.com/embed/ByXuk9QqQkk",
-      posterLink: "https://m.media-amazon.com/images/M/MV5BMjlmZmI5MDctNDE2YS00YWE0LWE5ZWItZDBhYWQ0NTcxNWRhXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg"
-  }
-];
 
-const flexboxContent = document.querySelector('.popular-movies-content');
 
-for (let i = 0; i < movies.length; i++) {
-  const divElement = document.createElement('div');
-  divElement.className = ` popular-movies-item popular-movies-item-${movies[i].id}`;
-  divElement.style.backgroundImage = `url(${movies[i].posterLink})`;
-  divElement.innerHTML = `
-  <div class="popular-movies-overlay">
-    <div class="description">
-      <h3>${movies[i].title}</h3>
-      <p>${movies[i].description}</p>
-      <p><strong>Genre:</strong> ${movies[i].genre}</p>
-      <p><strong>Age Restriction:</strong> ${movies[i].ageRestriction}</p>
-      <p>Run Time: ${movies[i].runTime} minutes</p>
-      <a href="${movies[i].trailerLink}" target="_blank">Watch Trailer</a>
-    </div>
-    <div class="overlay-textbox">
-      <p class="popular-movies-overlay-title">${movies[i].title}</p>
-      <p class="popular-movies-overlay-subtitle">${movies[i].description}</p>
-    </div>
-  </div>  
-  `;
-  flexboxContent.appendChild(divElement);
-}
 
 document.addEventListener('DOMContentLoaded', function() {
-    
-  const modal = document.getElementById('movieModal');
-  const close = document.querySelector('.close');
+      const close = document.querySelector('.close');
 
 
   window.addEventListener('click', function(event) {
     if (event.target == movieModal) {
         movieModal.style.display = 'none';
     }
-});
+  });
 
   close.onclick = function() {
       modal.style.display = "none";
@@ -160,50 +50,6 @@ document.addEventListener('DOMContentLoaded', function() {
           modal.style.display = "none";
       }
   }
-
-  const flexboxItems = document.querySelectorAll('.popular-movies-item');
-
-  flexboxItems.forEach((flexboxItem, index) => {
-      flexboxItem.addEventListener("click", function() {
-          // Populate modal with movie details
-            document.getElementById('modalPoster').src = movies[index].posterLink;
-            document.getElementById('modalTitle').textContent = movies[index].title;
-            document.getElementById('modalDescription').textContent = movies[index].description;
-            document.getElementById('modalGenre').innerHTML = "<strong>Genre:</strong> " + movies[index].genre;
-            document.getElementById('modalAgeRestriction').innerHTML = "<strong>Age:</strong> " + movies[index].ageRestriction;
-            document.getElementById('modalRunTime').innerHTML = "<strong>Run time:</strong> " + movies[index].runTime + " minutes";
-            document.getElementById('modalTrailer').src = movies[index].trailerLink;
-
-          // Display modal
-          modal.style.display = "block";
-
-          // Get the modal
-          var movieModal = document.getElementById("movieModal"); 
-
-          var span = document.querySelector("span.close");
-
-          
-            // When the user clicks on <span> (x), close the modal
-            span.onclick = function(event) {
-              console.log("Event handler called");
-              if (event.target == modal) {
-                console.log("Clicked outside the modal");
-                movieModal.style.display = "none";
-              }
-            }
-
-            // When the user clicks anywhere outside of the modal, close it
-            window.onclick = function(event) {
-              if (event.target == modal) {
-                movieModal.style.display = "none";
-              }
-            }
-          
-          
-      });
-  });
-
-  
 
   // Login Modal
 
---
+++ public/src/popular-movies/popular-movies.js
@@ -0,0 +1,59 @@
+export default class PopularMovies {
+    movies;
+
+    constructor(movies) {
+        this.movies = movies;
+    }
+
+    //from: https://www.youtube.com/watch?v=-FZ-pPFAjYY&ab_channel=FilmSelectTrailer
+    //to: https://www.youtube.com/embed/-FZ-pPFAjYY
+
+    _toEmbeddedLink = (link) => {
+        const url = "https://www.youtube.com/embed/";
+        const id = link.split("=")[1].split("&")[0];
+        return url + id;
+    }
+
+    show = async () => {
+        const flexboxContent = document.querySelector('.popular-movies-content');
+        const modal = document.getElementById('movieModal');
+        const self = this;
+        for (let i = 0; i < this.movies.length; i++) {
+            const divElement = document.createElement('div');
+            divElement.className = ` popular-movies-item popular-movies-item-${this.movies[i].id}`;
+            divElement.style.backgroundImage = `url(${this.movies[i].poster})`;
+            divElement.innerHTML = `
+                <div class="popular-movies-overlay">
+                    <div class="description">
+                        <h3>${this.movies[i].title}</h3>
+                        <p>${this.movies[i].plot}</p>
+                        <p><strong>Genre:</strong> ${this.movies[i].genre}</p>
+                        <p><strong>Age Restriction:</strong> ${this.movies[i].rated}</p>
+                        <p>Run Time: ${this.movies[i].runtime} minutes</p>
+                        <a href="${this.movies[i].trailerURL}" target="_blank">Watch Trailer</a>
+                    </div>
+                    <div class="overlay-textbox">
+                        <p class="popular-movies-overlay-title">${this.movies[i].title}</p>
+                        <p class="popular-movies-overlay-subtitle">${this.movies[i].plot}</p>
+                    </div>
+                </div>    
+            `;
+            (function (index) {
+                divElement.addEventListener("click", function() {
+                    window.addEventListener('click', (event) => {if (event.target == movieModal) movieModal.style.display = 'none'});
+                
+                    close.onclick = () => modal.style.display = "none";
+                
+                    window.onclick = (event) => {if (event.target == modal) modal.style.display = "none"};
+                    // Populate modal with movie details
+                    document.getElementById('modalPoster').src = self.movies[index].poster;
+                    document.getElementById('modalTitle').textContent = self.movies[index].title;
+                    document.getElementById('modalDescription').textContent = self.movies[index].plot;
+                    document.getElementById('modalTrailer').src = self._toEmbeddedLink(self.movies[index].trailerURL);
+                    modal.style.display = "block";
+            })})(i);
+            flexboxContent.appendChild(divElement);
+            if (i == 7) break; //allow max 8 movies
+        }
+    }
+} export { PopularMovies };
