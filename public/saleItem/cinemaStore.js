758b9ca7d45796d995e1d4ed664eaeab50cff59e
cihadozdemir <ciha0128@gmail.com>
2023-10-12 20:47:28 +0200

    gg

--- public/saleItem/cinemaStore.js
+++ public/saleItem/cinemaStore.js
@@ -1,28 +1,30 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const drinksContainer = document.getElementById('drinks-container');
-    const snacksContainer = document.getElementById('snacks-container');
-    const storeList = document.getElementById('store-list');
-    const totalElement = document.getElementById('total');
-    let total = 0.00;
+    const saleItemsContainer = document.getElementById('sale-items-container');
 
-    // Function to fetch sale items from the backend
-    function fetchSaleItems() {
-        fetch('/api/sales/sale-items') // Replace with your backend endpoint
-            .then(response => response.json())
-            .then(data => {
-                displaySaleItems(data);
+    // Function to fetch and display sale items
+    function fetchAndDisplaySaleItems() {
+        fetch('http://localhost:5555/api/sales/sale-items')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
             })
-            .catch(error => console.error('Error:', error));
+            .then(data => {
+                // Display sale items
+            })
+            .catch(error => {
+                console.error('Fetch error:', error);
+            });
     }
-
-    // Function to display sale items in the frontend
+    // Function to display sale items
     function displaySaleItems(saleItems) {
         saleItems.forEach(item => {
-            createAndDisplayItem(drinksContainer, item);
+            createAndDisplayItem(saleItemsContainer, item);
         });
     }
 
-    // Create and display item
+    // Create and display sale item
     function createAndDisplayItem(container, item) {
         const itemContainer = document.createElement('div');
         itemContainer.classList.add('flexbox-item');
@@ -30,44 +32,11 @@ document.addEventListener("DOMContentLoaded", function () {
         const itemName = document.createElement('span');
         itemName.textContent = `${item.saleItemName} - $${item.saleItemPrice.toFixed(2)}`;
 
-        const buyButton = document.createElement('button');
-        buyButton.textContent = 'Buy';
-        buyButton.addEventListener('click', () => {
-            const storeItem = document.createElement('li');
-            storeItem.textContent = item.saleItemName;
-            storeItem.dataset.cost = item.saleItemPrice;
-
-            const removeButton = document.createElement('button');
-            removeButton.textContent = 'Remove';
-            removeButton.addEventListener('click', () => {
-                storeList.removeChild(storeItem);
-                calculateTotal();
-            });
-
-            storeItem.appendChild(removeButton);
-            storeList.appendChild(storeItem);
-            calculateTotal();
-        });
-
         itemContainer.appendChild(itemName);
-        itemContainer.appendChild(buyButton);
         container.appendChild(itemContainer);
     }
 
-    function calculateTotal() {
-        total = 0.00;
-        storeList.querySelectorAll('li').forEach(item => {
-            const itemCost = parseFloat(item.dataset.cost);
-            if (!isNaN(itemCost)) {
-                total += itemCost;
-            }
-        });
-        totalElement.textContent = total.toFixed(2);
-    }
-
-    // Fetch sale items when the page loads
-    fetchSaleItems();
-
-    // Calculate total
-    calculateTotal();
+    // Fetch and display sale items when the page loads
+    fetchAndDisplaySaleItems();
+    console.log('JavaScript code is executed');
 });
