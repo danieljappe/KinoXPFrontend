a6ab874c379326bd9d52aaf370bb8f1eb5af564d
Nademtis <Mathias.99.madsen@gmail.com>
2023-10-12 13:25:59 +0200

    fixed bug in movie_schedule.js.

--- public/src/movie_schedule/movie_schedule.js
+++ public/src/movie_schedule/movie_schedule.js
@@ -43,14 +43,14 @@ export default class MovieSchedule {
     _createCard = function(showing1, showing2) {
         //initial card div
         const card = this.elementMaker.getDiv('date-card');
-    
+
         //get header for the card
         card.appendChild(this.elementMaker.getP('date-text date-text-box', showing1.date));
-    
+
         //make the movie-container for both movies
         card.appendChild(this._getMovieContainer(showing1, this.isMO));
         card.appendChild(this._getMovieContainer(showing2, this.isMO));
-    
+
         //add to list view
         if (!this.isMO) {
             this.scheduleDestination.appendChild(card);
@@ -61,29 +61,29 @@ export default class MovieSchedule {
 
     _getMovieContainer = function(showing) {
         const movieContainer = this.elementMaker.getDiv('movie-container');
-    
+
         //making a header, that display the time and theater
         const header = this.elementMaker.getDiv('movie-time-box');
         header.append(this.elementMaker.getP('movie-time-text', this._getTime(showing.theaterId, showing.time)));
         header.append(this.elementMaker.getP('movie-time-text', this._getTheater(showing.theaterId)));
         movieContainer.appendChild(header);
-    
+
         const movieBox = this.elementMaker.getDiv('movie-box');
-    
+
         //making a dark overlay with title and genre
         showing.theaterId !== 0? console.log(showing) : null;
         const overlay = this.elementMaker.getDiv('movie-overlay');
         overlay.appendChild(this.elementMaker.getP('movie-title', showing.movie.title));
         overlay.appendChild(this.elementMaker.getP('genre-title', showing.movie.genre));
         movieBox.appendChild(overlay);
-    
+
         //add image
         const img = document.createElement('img');
         img.className = 'poster-image';
         img.src = showing.movie.poster;
         img.alt = showing.movie.title;
         movieBox.appendChild(img);
-    
+
         //add buttons
         const buttons = this.elementMaker.getDiv('movie-action-buttons');
         if (!this.isMO) {
@@ -97,4 +97,4 @@ export default class MovieSchedule {
         //return the movie container
         return movieContainer;
     }
-} export { MovieSchedule };
+}; export { MovieSchedule };
