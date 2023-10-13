33ea24d018c24b282143ea583ddd3f09538743fe
fred077a <fred18q3@stud.kea.dk>
2023-10-13 10:09:38 +0200

    fixing fetcher for showings

--- public/src/dashboardJS/dashboard.js
+++ public/src/dashboardJS/dashboard.js
@@ -222,27 +222,11 @@ const movieRepository = new MovieRepository();
 
 //data
 const showingsNext3Months = await showingRepository.getAllShowings();
-const allMovies = await movieRepository.getAllMovies();
 for (let i = 0; i < showingsNext3Months.length; i++) {
   showingsNext3Months[i].addMovie(
     await movieRepository.getMovie(showingsNext3Months[i].movieId),
   );
 }
-
-//FOR TESTING
-/*
-const dateParser = new DateParser();
-const showingsToCreate = [
-  new Showing(0, 1, 2, "2023-10-16T15:00:00", dateParser),
-  new Showing(1, 2, 1, "2023-10-16T16:00:00", dateParser),
-  new Showing(2, 3, 2, "2023-10-17T17:00:00", dateParser),
-];
-
-for (let i = 0; i < showingsToCreate.length; i++) {
-  //showingRepository.createShowing(showingsToCreate[i]);
-}
-*/
-
 
 //add movie schedule
 const schedule = new Schedule(showingsNext3Months);
